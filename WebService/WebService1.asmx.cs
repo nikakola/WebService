1ab48038c5a4158b51f830bc6a09405b6984cfb6
veronika <nika.kolanda@gmail.com>
2023-02-27 19:23:40 +0300

    Add project files.

---
+++ WebService/WebService1.asmx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Description;
+using System.Web.Services.Protocols;
+using System.Xml.Serialization;
+
+namespace WebService
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        //[WebMethod]
+        //public string HelloWorld()
+        //{
+        //    return "Hello World";
+        //}
+        
+        [WebMethod]
+        [SoapDocumentMethod(ParameterStyle = SoapParameterStyle.Bare)]
+        [return: XmlElement("Message")]
+        public string IsPrime(int number)
+        {
+            if (number <= 1)
+            {
+                return "Number should be greater than 1.";
+            }
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                {
+                    return "Number is not a prime.";
+                }
+            }
+            return "Number is a prime";
+        }
+    }
+
+}
+
